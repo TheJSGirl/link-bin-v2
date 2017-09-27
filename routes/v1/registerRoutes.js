d17392e65ebb1a4600d209f8a803d0bfa67cc615
pnegi <prinegi100@gmail.com>
2017-09-27 18:08:48 +0530

    added bcrypt package and created POST route

--- routes/v1/registerRoutes.js
+++ routes/v1/registerRoutes.js
@@ -1,21 +1,62 @@
 const registerRoutes = require('express').Router();
 const pool = require('../../db');
+const  mysql = require('mysql2/promise');
+const bcrypt = require('bcrypt');
 
 registerRoutes.route('/')
   .get((req, res) => {
     
-    return res.send('GET Route');
+    return res.send('Home Page');
   })
-  .post((req, res) => {
-    const{name, email, pass, confirmPass} = req.body;
+  .post( async (req, res) => {
+    const{name, email, password} = req.body;
 
-    const userData = {
-      name,
-      email,
-      password,
-      confirmPass
+    if(!name || name.length < 5){
+      return res.status(422).json({
+        status: 'failed',
+        err: 'name is too short'
+      })
     }
-     return console.log(userData);
-  })
+
+    //validate email
+    if(!email){
+      return res.status(422).json({
+        status: 'failed',
+        err: 'email not mentioned'
+      })
+    }
+
+    //validate password
+    if(!password || password.length < 5){
+      return res.status(422).json({
+        status: 'failed',
+        err: 'password too small or password can not be null'
+      })
+    }
+
+    try{
+      
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const userData = {
+        name,
+        email,
+        password: hashedPassword
+      }
+      const data = await pool.query('INSERT INTO userDetails SET ?', userData);
+      console.log(data);
+
+      return res.status(200).json({
+        status: 'successful',
+        message: 'data saved successfully'
+      });
+
+    }     
+    catch(err){
+      return res.status(500).json({
+        status: 'failed',
+        err: 'something went wrong'
+      });
+    }
+  });
 
   module.exports = registerRoutes;
