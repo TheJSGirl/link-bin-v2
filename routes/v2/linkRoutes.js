de58b7a7624daf120754f1da0931467d4a8f6958
pnegi <prinegi100@gmail.com>
2017-10-02 18:14:42 +0530

    made custom response using join

--- routes/v2/linkRoutes.js
+++ routes/v2/linkRoutes.js
@@ -55,12 +55,71 @@ linkRoutes.route('/:id')
             return sendResponse(res, 422, [], 'invalid parameters');
         }
 
-        const id = parseInt(req.params.id);        
+        const id = parseInt(req.params.id); 
+
+        if(isNaN(id)){
+            return sendResponse(res, 422, [], 'invalid arguments');
+        }       
 
         try{
+            const query = `
+                select 
+
+                l.link, l.description, l.createdAt as linkCreationTime, 
+                u.name as linkOwner, ud.image as ownerImage,
+                c.commentedBy as commentOwnerId, ud2.image as commentOwnerImage, c.comm as commentBody,
+                u2.name as commentOwner, 
+                c.createdAt as commTime
+
+                from 
+
+                links l left join comments c on  l.id = c.commentedOn
+                inner join users u on l.createdBy = u.id
+                inner join user_details ud on l.createdBy = ud.userId
+                left join users u2 on c.commentedBy = u2.id
+                left join user_details ud2 on c.commentedBy = ud2.userId
+
+
+
+                where l.id = ${id}
+            `;
             
-            const userLinks = await pool.query(`SELECT links.link, links.description, links.createdBy, users.name FROM links inner join users on links.id = users.id`);
-            return sendResponse(res, 200, userLinks, 'data fetched successfully');
+            const [linkDetail] = await pool.query(query);
+
+            if(linkDetail.length === 0 ){
+                return sendResponse(res, 404, [], 'Data not found');
+            }
+
+            /**
+             * refine the data from DB or proper response 
+             */
+            const commentArr = [];
+
+            linkDetail.map((item) => {
+                const commentObj = {};
+                if(item.commentOwnerId){
+                    commentObj.userId = item.commentOwnerId;
+                    commentObj.userName = item.commentOwner;                
+                    commentObj.userImage = item.commentOwnerImage;
+                    commentObj.comment = item.commentBody;
+                    commentObj.time = item.commTime;               
+
+                    // push the commentobjectinto coment array
+                    commentArr.push(commentObj);
+                }
+            });
+
+            const responseData = {
+                linkId: id,
+                link: linkDetail[0].link,
+                description: linkDetail[0].description,
+                creationTime: linkDetail[0].linkCreationTime,
+                userName:linkDetail[0].linkOwner,
+                userImage: linkDetail[0].ownerImage,
+                comments: commentArr
+            }
+
+            return sendResponse(res, 200, responseData, 'data fetched successfully');
         }
         catch(err){
             console.log(err);
@@ -90,7 +149,7 @@ linkRoutes.route('/:id')
                 return sendResponse(res, 422, [], 'Invalid link');
             }
 
-            updateValues.push(`link = '${link}`);
+            updateValues.push(`link = '${link}'`);
         }
 
         if(description){
@@ -98,7 +157,7 @@ linkRoutes.route('/:id')
                 return sendResponse(res, 422, [], 'Invalid link');                
             }
 
-            updateValues.push(`description = '${description}`);            
+            updateValues.push(`description = '${description}'`);            
         }
 
         let updateQuery = 'UPDATE links SET ';
@@ -108,6 +167,7 @@ linkRoutes.route('/:id')
         }
 
         updateQuery += ` WHERE id = ${linkId}`;
+        console.log(updateQuery);
         
 
         try{
